4e2c3beb48a810a9719785e5e90329d561fe3924
AJ Futo <futoaj@gmail.com>
2022-07-28 20:39:19 -0700

    resume support

--- src/components/header.js
+++ src/components/header.js
@@ -7,11 +7,12 @@ class Header extends React.Component {
         return (
             <div className="container py-5">
                 <nav className="main-navbar d-flex flex-md-row flex-column navbar navbar-expand navbar-custom">
-                <span className="navbar-brand main-color-title">ajfu.to</span>
-                    <div className="d-flex flex-row">
+                <span className="navbar-brand main-color-title" style={{marginRight: 0}}>ajfu.to</span>
+                    <div className="d-flex flex-row" style={{marginLeft: 1 + "em"}}>
                         <Link className="nav-link" to="/">/home</Link>
                         <Link className="nav-link" to="/tech-tips">/tech-tips</Link>
                         <Link className="nav-link" to="/contact">/contact</Link>
+                        <a className="nav-link" href="../aj_futo_resume.pdf" target="_blank">/resume</a>
                         <ThemeToggle />
                     </div>
                 </nav>
---
+++ src/pages/resume.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class Resume extends React.Component {
+    render () {
+        return (
+            <meta http-equiv="Refresh" content="0; url='../aj_futo_resume.pdf'" />
+        )
+    }
+}
+
+export default Resume
