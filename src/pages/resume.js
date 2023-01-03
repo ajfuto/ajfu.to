56f3b4a9d8d3491f2f8b705033712eedc5796aee
A.J. Futo <futoaj@gmail.com>
2023-01-02 22:11:02 -0500

    updating resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,9 +1,13 @@
 import React from "react"
+import Seo from "../components/seo"
 
 class Resume extends React.Component {
     render () {
         return (
-            <meta http-equiv="Refresh" content="0; url='../aj_futo_resume.pdf'" />
+            <div>
+                <Seo title="Resume" description="AJ Futo's resume."/>
+                <meta http-equiv="Refresh" content="0; url='../aj_futo_resume.pdf'" />
+            </div>
         )
     }
 }
