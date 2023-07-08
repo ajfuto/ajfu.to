25292d9140751e229c8b7e22fd92881940cc1828
AJ Futo <futoaj@gmail.com>
2023-07-08 00:14:50 -0400

    updating resume & resume redirection

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,25 +1,9 @@
-import React from "react"
-import Seo from "../components/seo"
-import NavigationSpacer from "../components/navigation-spacer"
-import Footer from "../components/footer"
+import { navigate } from "@reach/router"
+import { useEffect } from "react"
 
-
-class Resume extends React.Component {
-    render () {
-        return (
-            <div className="main-page h-100">
-                <meta http-equiv="Refresh" content="0; url='../aj_futo_resume.pdf'" />
-                <Seo title="Resume" description="AJ Futo's resume."/>
-                <div className="container-fluid d-flex flex-column main-mh-100">
-                    <NavigationSpacer />
-                    <div className="container flex-grow-1">
-                        <p>You should automatically be redirected to my resume. If not, please click <a href='../aj_futo_resume.pdf'>here</a>.</p>
-                    </div>
-                    <Footer />
-                </div>
-            </div>
-        )
-    }
+export default function Resume() {
+  useEffect(() => {
+    navigate("https://ajfu.to/aj_futo_resume.pdf")
+  }, []);
+  return null;
 }
-
-export default Resume
